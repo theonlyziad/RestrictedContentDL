50ee0c2b75bd39f88805c4f5c07ecbe837757cd2
theonlyziad <132782844+theonlyziad@users.noreply.github.com>
2025-05-19 13:11:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-#Copyright (C) @TheSmartBisnu
-
-#Channel: https://t.me/itsSmartDev
-
 import os import shutil from time import time
 
 import psutil from pyrogram import Client, filters from pyrogram.enums import ParseMode from pyrogram.errors import PeerIdInvalid, BadRequest from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton from pyleaves import Leaves
